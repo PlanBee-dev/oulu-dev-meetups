18b70d8a4e222fc68e4ebe090816adf4be492566
Jörge Ojanen <jorge.mhnet@gmail.com>
2023-07-21 19:44:55 +0300

    added heroimage to mainpage

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Oulu developer meetups';
+export const SITE_DESCRIPTION = 'Listing of developer meetups in Oulu area';
