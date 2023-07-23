aafe1c90a3345cb7f5f112600d3c314def22c53a
Veeti K <veeti@veetik.com>
2023-07-23 21:01:47 +0300

    Update index.js

--- .github/workflows/new-meetup/lib/index.js
+++ .github/workflows/new-meetup/lib/index.js
@@ -718,7 +718,7 @@ var require_tunnel = __commonJS({
         connectOptions.headers = connectOptions.headers || {};
         connectOptions.headers["Proxy-Authorization"] = "Basic " + new Buffer(connectOptions.proxyAuth).toString("base64");
       }
-      debug("making CONNECT request");
+      debug2("making CONNECT request");
       var connectReq = self.request(connectOptions);
       connectReq.useChunkedEncodingByDefault = false;
       connectReq.once("response", onResponse);
@@ -738,7 +738,7 @@ var require_tunnel = __commonJS({
         connectReq.removeAllListeners();
         socket.removeAllListeners();
         if (res.statusCode !== 200) {
-          debug(
+          debug2(
             "tunneling socket could not be established, statusCode=%d",
             res.statusCode
           );
@@ -750,7 +750,7 @@ var require_tunnel = __commonJS({
           return;
         }
         if (head.length > 0) {
-          debug("got illegal response body from proxy");
+          debug2("got illegal response body from proxy");
           socket.destroy();
           var error = new Error("got illegal response body from proxy");
           error.code = "ECONNRESET";
@@ -758,13 +758,13 @@ var require_tunnel = __commonJS({
           self.removeSocket(placeholder);
           return;
         }
-        debug("tunneling connection has established");
+        debug2("tunneling connection has established");
         self.sockets[self.sockets.indexOf(placeholder)] = socket;
         return cb(socket);
       }
       function onError(cause) {
         connectReq.removeAllListeners();
-        debug(
+        debug2(
           "tunneling socket could not be established, cause=%s\n",
           cause.message,
           cause.stack
@@ -826,9 +826,9 @@ var require_tunnel = __commonJS({
       }
       return target;
     }
-    var debug;
+    var debug2;
     if (process.env.NODE_DEBUG && /\btunnel\b/.test(process.env.NODE_DEBUG)) {
-      debug = function() {
+      debug2 = function() {
         var args = Array.prototype.slice.call(arguments);
         if (typeof args[0] === "string") {
           args[0] = "TUNNEL: " + args[0];
@@ -838,10 +838,10 @@ var require_tunnel = __commonJS({
         console.error.apply(console, args);
       };
     } else {
-      debug = function() {
+      debug2 = function() {
       };
     }
-    exports.debug = debug;
+    exports.debug = debug2;
   }
 });
 
@@ -2127,10 +2127,10 @@ Support boolean input list: \`true | True | TRUE | false | False | FALSE\``);
       return process.env["RUNNER_DEBUG"] === "1";
     }
     exports.isDebug = isDebug;
-    function debug(message) {
+    function debug2(message) {
       command_1.issueCommand("debug", {}, message);
     }
-    exports.debug = debug;
+    exports.debug = debug2;
     function error(message, properties = {}) {
       command_1.issueCommand("error", utils_1.toCommandProperties(properties), message instanceof Error ? message.toString() : message);
     }
@@ -5505,7 +5505,7 @@ joinLink: ${joinLink}
 ` + description;
   await import_promises.default.writeFile("new-meetup.md", newMeetupFile);
   const newBranchName = "new-meetup-" + isoDate + "-" + meetupTitle;
-  const pullRequestTitle = "New meetup: " + issueTitle;
+  const pullRequestTitle = `New meetup: ${issueTitle}`;
   const pullRequestBody = `New meetup: ${issueTitle}
 
 Date: ${date}
@@ -5520,6 +5520,29 @@ Closes #${issueNumber}`;
   core.setOutput("branch_name", newBranchName);
   core.setOutput("pull_request_title", pullRequestTitle);
   core.setOutput("pull_request_body", pullRequestBody);
+  core.debug(
+    JSON.stringify(
+      {
+        branch_name: newBranchName,
+        pull_request_title: pullRequestTitle,
+        pull_request_body: pullRequestBody,
+        issueTitle,
+        issueBody,
+        issueNumber,
+        date,
+        isoDate,
+        location,
+        locationLinkGoogleMaps,
+        organiser,
+        organiserLink,
+        joinLink,
+        description,
+        newMeetupFile
+      },
+      null,
+      4
+    )
+  );
   core.info("Done");
 }
 main();
