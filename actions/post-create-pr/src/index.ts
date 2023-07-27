724e12a64bbe8c721f30a68a3944b1abbc278cd7
Veeti K <veeti@veetik.com>
2023-07-27 16:46:50 +0300

    New meetup action (#19)
    * Create new-event.yml
    
    * english
    
    * new event workflow
    
    * Update new-event.yml
    
    * test
    
    * Update new-event.yml
    
    * Update new-event.yml
    
    * Update new-event.yml
    
    * wip
    
    * Update new-event.yml
    
    * Update new-event.yml
    
    * wip
    
    * Update new-event.yml
    
    * Update new-event.yml
    
    * Update new-event.yml
    
    * Update new-event.yml
    
    * Update new-event.yml
    
    * Update new-event.yml
    
    * Update new-event.yml
    
    * Update new-event.yml
    
    * Update new-event.yml
    
    * Update new-event.yml
    
    * Update new-event.yml
    
    * Update new-event.yml
    
    * Update new-event.yml
    
    * Update new-event.yml
    
    * test
    
    * Update action.yml
    
    * Update new-meetup.yml
    
    * esbuild
    
    * logic with nodejs action
    
    * Update action.yml
    
    * Update index.js
    
    * Update index.ts
    
    * Update index.js
    
    * Update new-meetup.yml
    
    * fix
    
    * clean
    
    * folder
    
    * filename
    
    * Update action.yml
    
    * up
    
    * Update new-meetup.yml
    
    * a
    
    * up
    
    * up
    
    * up
    
    * up
    
    * up
    
    * up
    
    * Update new-meetup.yml
    
    * up
    
    * up
    
    * up
    
    * up
    
    * fix msg
    
    * tsx?
    
    * up
    
    * up
    
    * up
    
    * up
    
    * :D
    
    * Update action.yml
    
    * Update action.yml
    
    * Update action.yml
    
    * up
    
    * Update new-meetup.yml
    
    * up
    
    * Update new-meetup.yml
    
    * Update new-meetup.yml
    
    * Update package.json
    
    * move
    
    * move actions to root
    
    * run `pnpm lint --fix`

---
+++ actions/post-create-pr/src/index.ts
@@ -0,0 +1,30 @@
+import { context, getOctokit } from '@actions/github';
+import { z } from 'zod';
+
+const envSchema = z.object({
+  PULL_REQUEST_NUMBER: z.string().transform(Number),
+  COMMENT_ID: z.string().transform(Number),
+  GITHUB_TOKEN: z.string(),
+});
+
+async function main() {
+  const env = envSchema.parse(process.env);
+
+  const octokit = getOctokit(env.GITHUB_TOKEN);
+
+  await octokit.rest.issues.updateComment({
+    comment_id: env.COMMENT_ID,
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    body: `
+Hi there! Thanks for creating a new meetup. I'm going to create a new branch and pull request for you.
+
+1. Validating meetup details... Done! ✅
+2. Creating meetup file... Done! ✅
+3. Creating new branch and pull request... Done! ✅ 
+
+Here's the new pull request: #${env.PULL_REQUEST_NUMBER}`,
+  });
+}
+
+main();
