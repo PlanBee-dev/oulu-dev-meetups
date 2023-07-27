73f8d1cbcd54786f15420bd19d81cf3fea655c96
Veeti K <veeti@veetik.com>
2023-07-27 16:41:10 +0300

    run `pnpm lint --fix`

--- actions/new-meetup/src/index.ts
+++ actions/new-meetup/src/index.ts
@@ -1,157 +1,163 @@
-import * as core from "@actions/core";
-import { context, getOctokit } from "@actions/github";
-import format from "date-fns/format";
-import isValid from "date-fns/isValid";
-import parse from "date-fns/parse";
-import fs from "fs/promises";
-import { z } from "zod";
+import * as core from '@actions/core';
+import { context, getOctokit } from '@actions/github';
+import format from 'date-fns/format';
+import isValid from 'date-fns/isValid';
+import parse from 'date-fns/parse';
+import fs from 'fs/promises';
+import { z } from 'zod';
 
 const envSchema = z.object({
-	MEETUP_FOLDER: z.string(),
-	ISSUE_TITLE: z.string(),
-	ISSUE_BODY: z.string(),
-	ISSUE_NUMBER: z.string().transform(Number),
-	GITHUB_TOKEN: z.string(),
+  MEETUP_FOLDER: z.string(),
+  ISSUE_TITLE: z.string(),
+  ISSUE_BODY: z.string(),
+  ISSUE_NUMBER: z.string().transform(Number),
+  GITHUB_TOKEN: z.string(),
 });
 
 async function main() {
-	const env = envSchema.parse(process.env);
+  const env = envSchema.parse(process.env);
 
-	const octokit = getOctokit(env.GITHUB_TOKEN);
+  const octokit = getOctokit(env.GITHUB_TOKEN);
 
-	const createCommentResponse = await octokit.rest.issues.createComment({
-		owner: context.repo.owner,
-		repo: context.repo.repo,
-		title: `New meetup: ${env.ISSUE_TITLE}`,
-		issue_number: env.ISSUE_NUMBER,
-		body: getStatusMessage(["loading", "idle", "idle"]),
-	});
+  const createCommentResponse = await octokit.rest.issues.createComment({
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    title: `New meetup: ${env.ISSUE_TITLE}`,
+    issue_number: env.ISSUE_NUMBER,
+    body: getStatusMessage(['loading', 'idle', 'idle']),
+  });
 
-	core.setOutput("comment_id", createCommentResponse.data.id);
+  core.setOutput('comment_id', createCommentResponse.data.id);
 
-	const sanitizedMeetupTitle = sanitizeString(env.ISSUE_TITLE);
+  const sanitizedMeetupTitle = sanitizeString(env.ISSUE_TITLE);
 
-	const date = env.ISSUE_BODY.match(getTitleParsingRegex("Time and date"))?.[1];
-	const location = env.ISSUE_BODY.match(getTitleParsingRegex("Location"))?.[1];
-	const locationLinkGoogleMaps = env.ISSUE_BODY.match(
-		getTitleParsingRegex("Location as a Google Maps link")
-	)?.[1];
-	const organiser = env.ISSUE_BODY.match(getTitleParsingRegex("Organiser"))?.[1];
-	const organiserLink = env.ISSUE_BODY.match(getTitleParsingRegex("Link to organiser"))?.[1];
-	const joinLink = env.ISSUE_BODY.match(getTitleParsingRegex("Joining link"))?.[1];
+  const date = env.ISSUE_BODY.match(getTitleParsingRegex('Time and date'))?.[1];
+  const location = env.ISSUE_BODY.match(getTitleParsingRegex('Location'))?.[1];
+  const locationLinkGoogleMaps = env.ISSUE_BODY.match(
+    getTitleParsingRegex('Location as a Google Maps link'),
+  )?.[1];
+  const organiser = env.ISSUE_BODY.match(
+    getTitleParsingRegex('Organiser'),
+  )?.[1];
+  const organiserLink = env.ISSUE_BODY.match(
+    getTitleParsingRegex('Link to organiser'),
+  )?.[1];
+  const joinLink = env.ISSUE_BODY.match(
+    getTitleParsingRegex('Joining link'),
+  )?.[1];
 
-	const description = getDescription(env.ISSUE_BODY);
+  const description = getDescription(env.ISSUE_BODY);
 
-	if (
-		!date ||
-		!location ||
-		!locationLinkGoogleMaps ||
-		!organiser ||
-		!organiserLink ||
-		!joinLink ||
-		!description
-	) {
-		core.debug(`Date: ${date}`);
-		core.debug(`Location: ${location}`);
-		core.debug(`Location link: ${locationLinkGoogleMaps}`);
-		core.debug(`Organiser: ${organiser}`);
-		core.debug(`Organiser link: ${organiserLink}`);
-		core.debug(`Join link: ${joinLink}`);
-		core.debug(`Description: ${description}`);
-		core.debug(`Issue body: ${env.ISSUE_BODY}`);
+  if (
+    !date ||
+    !location ||
+    !locationLinkGoogleMaps ||
+    !organiser ||
+    !organiserLink ||
+    !joinLink ||
+    !description
+  ) {
+    core.debug(`Date: ${date}`);
+    core.debug(`Location: ${location}`);
+    core.debug(`Location link: ${locationLinkGoogleMaps}`);
+    core.debug(`Organiser: ${organiser}`);
+    core.debug(`Organiser link: ${organiserLink}`);
+    core.debug(`Join link: ${joinLink}`);
+    core.debug(`Description: ${description}`);
+    core.debug(`Issue body: ${env.ISSUE_BODY}`);
 
-		await octokit.rest.issues.updateComment({
-			comment_id: createCommentResponse.data.id,
-			owner: context.repo.owner,
-			repo: context.repo.repo,
-			body: getStatusMessage(["error", "idle", "idle"]),
-		});
+    await octokit.rest.issues.updateComment({
+      comment_id: createCommentResponse.data.id,
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      body: getStatusMessage(['error', 'idle', 'idle']),
+    });
 
-		core.setFailed("Invalid issue body");
+    core.setFailed('Invalid issue body');
 
-		return;
-	}
+    return;
+  }
 
-	const parsedDate = parse(date, "dd-MM-yyyy HH:mm", new Date());
-	if (!isValid(parsedDate)) {
-		await octokit.rest.issues.updateComment({
-			comment_id: createCommentResponse.data.id,
-			owner: context.repo.owner,
-			repo: context.repo.repo,
-			body: getStatusMessage(["error", "idle", "idle"]),
-		});
+  const parsedDate = parse(date, 'dd-MM-yyyy HH:mm', new Date());
+  if (!isValid(parsedDate)) {
+    await octokit.rest.issues.updateComment({
+      comment_id: createCommentResponse.data.id,
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      body: getStatusMessage(['error', 'idle', 'idle']),
+    });
 
-		core.setFailed("Invalid date");
-		return;
-	}
+    core.setFailed('Invalid date');
+    return;
+  }
 
-	await octokit.rest.issues.updateComment({
-		comment_id: createCommentResponse.data.id,
-		owner: context.repo.owner,
-		repo: context.repo.repo,
-		body: getStatusMessage(["success", "loading", "idle"]),
-	});
-	const isoDate = parsedDate.toISOString();
-	const sanitizedDate = format(parsedDate, "dd-MM-yyyy-HH-mm");
+  await octokit.rest.issues.updateComment({
+    comment_id: createCommentResponse.data.id,
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    body: getStatusMessage(['success', 'loading', 'idle']),
+  });
+  const isoDate = parsedDate.toISOString();
+  const sanitizedDate = format(parsedDate, 'dd-MM-yyyy-HH-mm');
 
-	const newMeetupFile = getMeetupFileContent({
-		isoDate,
-		organiser,
-		organiserLink,
-		location,
-		locationLinkGoogleMaps,
-		issueNumber: env.ISSUE_NUMBER,
-		issueTitle: env.ISSUE_TITLE,
-		description,
-		joinLink,
-	});
+  const newMeetupFile = getMeetupFileContent({
+    isoDate,
+    organiser,
+    organiserLink,
+    location,
+    locationLinkGoogleMaps,
+    issueNumber: env.ISSUE_NUMBER,
+    issueTitle: env.ISSUE_TITLE,
+    description,
+    joinLink,
+  });
 
-	await fs.writeFile(
-		`./${env.MEETUP_FOLDER}/${sanitizedMeetupTitle}-${sanitizedDate}.md`,
-		newMeetupFile
-	);
+  await fs.writeFile(
+    `./${env.MEETUP_FOLDER}/${sanitizedMeetupTitle}-${sanitizedDate}.md`,
+    newMeetupFile,
+  );
 
-	await octokit.rest.issues.updateComment({
-		comment_id: createCommentResponse.data.id,
-		owner: context.repo.owner,
-		repo: context.repo.repo,
-		body: getStatusMessage(["success", "success", "loading"]),
-	});
+  await octokit.rest.issues.updateComment({
+    comment_id: createCommentResponse.data.id,
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    body: getStatusMessage(['success', 'success', 'loading']),
+  });
 
-	const newBranchName = `new-meetup-${sanitizedMeetupTitle}-${sanitizedDate}`;
+  const newBranchName = `new-meetup-${sanitizedMeetupTitle}-${sanitizedDate}`;
 
-	const pullRequestTitle = `New meetup: ${env.ISSUE_TITLE}`;
-	const pullRequestBody = getPullRequestBody({
-		isoDate,
-		organiser,
-		organiserLink,
-		location,
-		locationLinkGoogleMaps,
-		issueNumber: env.ISSUE_NUMBER,
-	});
+  const pullRequestTitle = `New meetup: ${env.ISSUE_TITLE}`;
+  const pullRequestBody = getPullRequestBody({
+    isoDate,
+    organiser,
+    organiserLink,
+    location,
+    locationLinkGoogleMaps,
+    issueNumber: env.ISSUE_NUMBER,
+  });
 
-	core.setOutput("branch_name", newBranchName);
-	core.setOutput("pull_request_title", pullRequestTitle);
-	core.setOutput("pull_request_body", pullRequestBody);
+  core.setOutput('branch_name', newBranchName);
+  core.setOutput('pull_request_title', pullRequestTitle);
+  core.setOutput('pull_request_body', pullRequestBody);
 
-	core.info("Done");
+  core.info('Done');
 }
 
 main();
 
 function sanitizeString(str: string) {
-	return str.replace(/[^a-z0-9]/gi, "-").toLowerCase();
+  return str.replace(/[^a-z0-9]/gi, '-').toLowerCase();
 }
 
 function getPullRequestBody(props: {
-	isoDate: string;
-	organiser: string;
-	organiserLink: string;
-	location: string;
-	locationLinkGoogleMaps: string;
-	issueNumber: number;
+  isoDate: string;
+  organiser: string;
+  organiserLink: string;
+  location: string;
+  locationLinkGoogleMaps: string;
+  issueNumber: number;
 }) {
-	return `
+  return `
 New meetup
 
 Date:
@@ -167,17 +173,17 @@ Closes #${props.issueNumber}`;
 }
 
 function getMeetupFileContent(props: {
-	isoDate: string;
-	organiser: string;
-	organiserLink: string;
-	location: string;
-	locationLinkGoogleMaps: string;
-	issueNumber: number;
-	issueTitle: string;
-	description: string;
-	joinLink: string;
+  isoDate: string;
+  organiser: string;
+  organiserLink: string;
+  location: string;
+  locationLinkGoogleMaps: string;
+  issueNumber: number;
+  issueTitle: string;
+  description: string;
+  joinLink: string;
 }) {
-	return `
+  return `
 ---
 date: "${props.isoDate}"
 location: "${props.location}"
@@ -193,50 +199,50 @@ ${props.description}`;
 }
 
 function getTitleParsingRegex(title: string) {
-	return new RegExp(`### ${title}\\s*\\n\\s*([\\s\\S]*?)\\s*\\n\\s*###`);
+  return new RegExp(`### ${title}\\s*\\n\\s*([\\s\\S]*?)\\s*\\n\\s*###`);
 }
 
 function getDescription(issueBody: string) {
-	const targetPhrase = "### Description";
+  const targetPhrase = '### Description';
 
-	const descriptionIndex = issueBody.indexOf(targetPhrase);
-	if (descriptionIndex === -1) {
-		return null;
-	}
+  const descriptionIndex = issueBody.indexOf(targetPhrase);
+  if (descriptionIndex === -1) {
+    return null;
+  }
 
-	const resultString = issueBody.slice(descriptionIndex + targetPhrase.length);
-	return resultString.trim();
+  const resultString = issueBody.slice(descriptionIndex + targetPhrase.length);
+  return resultString.trim();
 }
 
-type Status = "loading" | "success" | "error" | "idle";
+type Status = 'loading' | 'success' | 'error' | 'idle';
 
 function getStatusMessage(status: readonly [Status, Status, Status]) {
-	const firstMessage =
-		status[0] === "idle"
-			? "Validating meetup details..."
-			: status[0] === "loading"
-			? "Validating meetup details..."
-			: status[0] === "success"
-			? "Validating meetup details... Done! ✅"
-			: "Validating meetup details... Failed! ❌";
+  const firstMessage =
+    status[0] === 'idle'
+      ? 'Validating meetup details...'
+      : status[0] === 'loading'
+      ? 'Validating meetup details...'
+      : status[0] === 'success'
+      ? 'Validating meetup details... Done! ✅'
+      : 'Validating meetup details... Failed! ❌';
 
-	const secondMessage =
-		status[1] === "idle"
-			? "Creating meetup file..."
-			: status[1] === "loading"
-			? "Creating meetup file..."
-			: status[1] === "success"
-			? "Creating meetup file... Done! ✅"
-			: "Creating meetup file... Failed! ❌";
+  const secondMessage =
+    status[1] === 'idle'
+      ? 'Creating meetup file...'
+      : status[1] === 'loading'
+      ? 'Creating meetup file...'
+      : status[1] === 'success'
+      ? 'Creating meetup file... Done! ✅'
+      : 'Creating meetup file... Failed! ❌';
 
-	const thirdMessage =
-		status[2] === "idle"
-			? "Create new branch and pull request"
-			: status[2] === "loading"
-			? "Creating new branch and pull request..."
-			: null;
+  const thirdMessage =
+    status[2] === 'idle'
+      ? 'Create new branch and pull request'
+      : status[2] === 'loading'
+      ? 'Creating new branch and pull request...'
+      : null;
 
-	return `
+  return `
 Hi there! Thanks for creating a new meetup. I'm going to create a new branch and pull request for you.
 
 1. ${firstMessage}
--- actions/post-create-pr/src/index.ts
+++ actions/post-create-pr/src/index.ts
@@ -1,22 +1,22 @@
-import { context, getOctokit } from "@actions/github";
-import { z } from "zod";
+import { context, getOctokit } from '@actions/github';
+import { z } from 'zod';
 
 const envSchema = z.object({
-	PULL_REQUEST_NUMBER: z.string().transform(Number),
-	COMMENT_ID: z.string().transform(Number),
-	GITHUB_TOKEN: z.string(),
+  PULL_REQUEST_NUMBER: z.string().transform(Number),
+  COMMENT_ID: z.string().transform(Number),
+  GITHUB_TOKEN: z.string(),
 });
 
 async function main() {
-	const env = envSchema.parse(process.env);
+  const env = envSchema.parse(process.env);
 
-	const octokit = getOctokit(env.GITHUB_TOKEN);
+  const octokit = getOctokit(env.GITHUB_TOKEN);
 
-	await octokit.rest.issues.updateComment({
-		comment_id: env.COMMENT_ID,
-		owner: context.repo.owner,
-		repo: context.repo.repo,
-		body: `
+  await octokit.rest.issues.updateComment({
+    comment_id: env.COMMENT_ID,
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    body: `
 Hi there! Thanks for creating a new meetup. I'm going to create a new branch and pull request for you.
 
 1. Validating meetup details... Done! ✅
@@ -24,7 +24,7 @@ Hi there! Thanks for creating a new meetup. I'm going to create a new branch and
 3. Creating new branch and pull request... Done! ✅ 
 
 Here's the new pull request: #${env.PULL_REQUEST_NUMBER}`,
-	});
+  });
 }
 
 main();
