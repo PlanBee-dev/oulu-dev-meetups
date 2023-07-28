5ca817966a1ab2c5115b553e60f7ab798c046d1b
AriPerkkio <ari.perkkio@gmail.com>
2023-07-28 15:04:54 +0300

    test: vitest workspace

---
+++ packages/site/test/index.test.ts
@@ -0,0 +1,29 @@
+import { Browser, chromium } from 'playwright';
+import { afterAll, beforeAll, expect, test } from 'vitest';
+
+let browser: Browser;
+beforeAll(async () => {
+  browser = await chromium.launch({ headless: true });
+});
+afterAll(async () => {
+  await browser?.close();
+});
+
+test('meta is correct', async () => {
+  const page = await browser.newPage();
+  page.on('console', (msg) => console.log(msg.text()));
+
+  await page.goto('http://localhost:3000/oulu-dev-meetups/');
+
+  const title = await page.title();
+  expect(title).toBe('Oulu developer meetups');
+});
+
+test('main heading is set', async () => {
+  const page = await browser.newPage();
+  page.on('console', (msg) => console.log(msg.text()));
+
+  await page.goto('http://localhost:3000/oulu-dev-meetups/');
+
+  page.getByRole('heading', { level: 1, name: 'Oulu developer meetups' });
+});
