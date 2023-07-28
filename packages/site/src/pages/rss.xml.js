9ef406f7729993c3155bba55739a74f1fb20a250
Jörge Ojanen <jorge.mhnet@gmail.com>
2023-07-28 14:32:32 +0300

    give rss data in correct format

--- packages/site/src/pages/rss.xml.js
+++ packages/site/src/pages/rss.xml.js
@@ -4,13 +4,15 @@ import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
 
 export async function get(context) {
   const meetups = await getCollection('meetups');
+  const rssItems = meetups.map((meetup) => ({
+    title: meetup.data.name,
+    link: `${import.meta.env.BASE_URL}meetups/${meetup.slug}`,
+    pubDate: meetup.data.time
+  }));
   return rss({
     title: SITE_TITLE,
     description: SITE_DESCRIPTION,
     site: context.site,
-    items: meetups.map((meetup) => ({
-      ...meetup.data,
-      link: `/meetups/${meetup.slug}/`,
-    })),
+    items: rssItems,
   });
 }
