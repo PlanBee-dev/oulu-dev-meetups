bef3270871217a43723d4395b3179f46568f69f2
Veeti K <veeti@veetik.com>
2023-07-29 18:29:16 +0300

    start wrangler dev in test

--- packages/api/test/cors.test.ts
+++ packages/api/test/cors.test.ts
@@ -1,80 +1,61 @@
-import { afterAll, beforeAll, describe, expect, test } from 'vitest';
-import { UnstableDevWorker, unstable_dev } from 'wrangler';
-import { testEnvVars } from './_test.env-vars';
+import { expect, test } from 'vitest';
 
-describe('cors test', () => {
-  let worker: UnstableDevWorker;
+test('answers preflight request with cors stuff', async () => {
+  const allowedOrigin = 'http://localhost:3000';
 
-  beforeAll(async () => {
-    worker = await unstable_dev('./packages/api/src/worker.ts', {
-      experimental: {
-        disableExperimentalWarning: true,
+  const res = await fetch('http://127.0.0.1:8787', {
+    method: 'OPTIONS',
+    headers: { origin: allowedOrigin },
+  });
+
+  expect(res.status).toBe(204);
+  expect(res.headers.get('Access-Control-Allow-Origin')).toBe(allowedOrigin);
+  expect(res.headers.get('Access-Control-Allow-Methods')).toBe(
+    'POST, OPTIONS, HEAD',
+  );
+  expect(res.headers.get('Access-Control-Allow-Headers')).toBe(
+    'Content-Type, Accept, Origin',
+  );
+});
+
+test('Invalid json', async () => {
+  const res = await fetch('http://127.0.0.1:8787', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ name: 'world' }),
+  });
+
+  expect(res.status).toBe(400);
+  expect(await res.json()).toEqual({
+    error: {
+      message: 'Invalid JSON - schema parsing failed',
+      details: {
+        date: ['Required'],
+        description: ['Required'],
+        location: ['Required'],
+        locationLink: ['Required'],
+        organizer: ['Required'],
+        organizerLink: ['Required'],
+        signupLink: ['Required'],
+        time: ['Required'],
+        title: ['Required'],
       },
-      vars: testEnvVars,
-    });
+    },
+  });
+});
+
+test('Malformed JSON', async () => {
+  const res = await fetch('http://127.0.0.1:8787', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: '{',
   });
 
-  afterAll(() => {
-    void worker.stop();
-  });
-
-  test('answers preflight request with cors stuff', async () => {
-    const allowedOrigin = 'http://localhost:3000';
-
-    const res = await worker.fetch('http://localhost:3000', {
-      method: 'OPTIONS',
-      headers: { origin: allowedOrigin },
-    });
-
-    expect(res.status).toBe(204);
-    expect(res.headers.get('Access-Control-Allow-Origin')).toBe(allowedOrigin);
-    expect(res.headers.get('Access-Control-Allow-Methods')).toBe(
-      'POST, OPTIONS, HEAD',
-    );
-    expect(res.headers.get('Access-Control-Allow-Headers')).toBe(
-      'Content-Type, Accept, Origin',
-    );
-  });
-
-  test('Invalid json', async () => {
-    const res = await worker.fetch('http://localhost:3000', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ name: 'world' }),
-    });
-
-    expect(res.status).toBe(400);
-    expect(await res.json()).toEqual({
-      error: {
-        message: 'Invalid JSON - schema parsing failed',
-        details: {
-          date: ['Required'],
-          description: ['Required'],
-          location: ['Required'],
-          locationLink: ['Required'],
-          organizer: ['Required'],
-          organizerLink: ['Required'],
-          signupLink: ['Required'],
-          time: ['Required'],
-          title: ['Required'],
-        },
-      },
-    });
-  });
-
-  test('Malformed JSON', async () => {
-    const res = await worker.fetch('http://localhost:3000', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: '{',
-    });
-
-    expect(res.status).toBe(400);
-    expect(await res.json()).toEqual({
-      error: {
-        message: 'Invalid JSON - catched an error',
-        details: {},
-      },
-    });
+  expect(res.status).toBe(400);
+  expect(await res.json()).toEqual({
+    error: {
+      message: 'Invalid JSON - catched an error',
+      details: {},
+    },
   });
 });
--- packages/api/vitest.config.ts
+++ packages/api/vitest.config.ts
@@ -1,5 +1,13 @@
+import { resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
 import { defineConfig } from 'vitest/config';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = resolve(__filename, '..');
+
 export default defineConfig({
-  test: { name: 'api' },
+  test: {
+    name: 'api',
+    globalSetup: resolve(__dirname, './test/setup-worker.ts'),
+  },
 });
