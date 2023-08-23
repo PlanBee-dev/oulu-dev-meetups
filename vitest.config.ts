9a0d784605edad76b2ad5a05b49168d58281255b
Veeti K <veeti@veetik.com>
2023-08-23 21:32:59 +0300

    hide(?) console.errors

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,5 +3,15 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     reporters: process.env.CI ? 'default' : 'verbose',
+    onConsoleLog(message, level) {
+      if (
+        level === 'stderr' &&
+        ['Invalid JSON - ', 'Failed to create issue -'].some((str) =>
+          message.includes(str),
+        )
+      ) {
+        return false;
+      }
+    },
   },
 });
