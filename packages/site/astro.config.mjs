fb022d3f295012772538ea7ead16204fc8708876
Jörge Ojanen <jorge.mhnet@gmail.com>
2023-08-27 21:03:58 +0300

    added remark plugin

--- packages/site/astro.config.mjs
+++ packages/site/astro.config.mjs
@@ -1,12 +1,16 @@
-import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import remarkGfm from 'remark-gfm';
 
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://PlanBee-dev.github.io",
-  base: "/oulu-dev-meetups",
+  site: 'https://PlanBee-dev.github.io',
+  base: '/oulu-dev-meetups',
   integrations: [mdx(), sitemap(), tailwind()],
+  markdown: {
+    remarkPlugins: [remarkGfm],
+  },
 });
