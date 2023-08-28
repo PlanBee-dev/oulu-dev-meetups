3e92956c189f1ae9ac39fd136587920ba16c3fef
Jörge Ojanen <jorge.mhnet@gmail.com>
2023-08-28 20:34:52 +0300

    Revert "added remark plugin"
    This reverts commit a815b7100de1c2d334ff6931520de8db227c55eb.

--- packages/site/astro.config.mjs
+++ packages/site/astro.config.mjs
@@ -1,16 +1,12 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import remarkGfm from 'remark-gfm';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://PlanBee-dev.github.io',
-  base: '/oulu-dev-meetups',
+  site: "https://PlanBee-dev.github.io",
+  base: "/oulu-dev-meetups",
   integrations: [mdx(), sitemap(), tailwind()],
-  markdown: {
-    remarkPlugins: [remarkGfm],
-  },
 });
