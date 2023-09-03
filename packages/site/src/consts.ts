ec594507975aae37144ba71c2faa647755ba74eb
Jörge Ojanen <jorge.mhnet@gmail.com>
2023-09-03 21:24:31 +0300

    add testmeetup, sort meetups in listing

--- packages/site/src/consts.ts
+++ packages/site/src/consts.ts
@@ -3,3 +3,12 @@
 
 export const SITE_TITLE = 'Oulu developer meetups';
 export const SITE_DESCRIPTION = 'Listing of developer meetups in Oulu area';
+
+export const getRandomLogonumber = () => {
+  const allowedNums = [1, 2, 3, 4, 5];
+  let devLogoNum = Math.floor(Math.random() * 5) + 1;
+  if (!allowedNums.includes(devLogoNum)) {
+    devLogoNum = 1;
+  }
+  return devLogoNum;
+};
