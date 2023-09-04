d83379cafd1efdec8e68aa20eb1d1dccc1467b65
Jörge Ojanen <jorge.mhnet@gmail.com>
2023-09-04 23:00:56 +0300

    34 add testmeetups folder to use as a testcontent for local development (#38)
    * added meetups testfolder for local dev
    
    * keep empty meetups folder
    
    * typescript just keeps on slandering about meetup types
    
    * add testmeetup, sort meetups in listing
    
    * rename test meetup file so sorting should have effect
    
    * added date parsing and sorting, for user and system dates. and test.
    
    * add still back testmeetup in meetups folder, for a while
    
    * added test
    
    * yea dont test as localdates on github, dummy
    
    * damnit!
    
    * oh ffs
    
    * dont show empty card on front page in no coming meetups

--- packages/site/src/content/config.ts
+++ packages/site/src/content/config.ts
@@ -16,4 +16,30 @@ const meetups = defineCollection({
   }),
 });
 
-export const collections = { meetups };
+const testmeetups = defineCollection({
+  type: 'content',
+  schema: z.object({
+    name: z.string(),
+    description: z.string(),
+    // Transform string to Date object
+    time: z.string(),
+    location: z.string(),
+    locationLink: z.string().optional(),
+    organizer: z.string(),
+    organizerLink: z.string().optional(),
+    meetupLink: z.string(),
+    image: z.string().optional(),
+  }),
+});
+export const MEETUPS = 'meetups';
+export const TEST_MEETUPS = 'testmeetups';
+
+export const collections = { meetups, testmeetups };
+
+export type CollectionName = 'meetups' | 'testmeetups';
+
+export const getMeetupsCollectionName = () => {
+  if (process.env.NODE_ENV === 'production') {
+    return MEETUPS;
+  } else return TEST_MEETUPS;
+};
---
+++ packages/site/test/utils.test.ts
@@ -0,0 +1,94 @@
+import {
+  Meetup,
+  getNextMeetup,
+  getRandomLogonumber,
+  parseMeetupDate,
+} from '../src/utils';
+import { expect, test, beforeEach, afterEach, vi } from 'vitest';
+
+beforeEach(() => {
+  vi.useFakeTimers();
+});
+
+afterEach(() => {
+  vi.useRealTimers();
+});
+
+const meetups: Meetup[] = [];
+meetups.push({
+  data: {
+    name: 'React coding Meetup',
+    description: 'Monthly meetup for coders',
+    location: 'Oulu Library',
+    locationLink: null,
+    time: '2023-09-25 17:00',
+    organizer: 'React Community',
+    organizerLink: null,
+    meetupLink: 'https://www.meetup.com/react',
+    image: 'react-meetup.jpg',
+  },
+});
+meetups.push({
+  data: {
+    name: 'Vitest Meetup',
+    description: 'Monthly meetup for Vitest users',
+    location: 'Oulu University',
+    locationLink: null,
+    time: '2023-08-25 18:00',
+    organizer: 'Vitest Community',
+    organizerLink: null,
+    meetupLink: 'https://www.meetup.com/vitest',
+    image: 'vitest-meetup.jpg',
+  },
+});
+meetups.push({
+  data: {
+    name: 'Local AWS group Meetup',
+    location: 'Oulu Elektroniikkatie',
+    locationLink: 'https://goo.gl/maps/GvuboievRSW5VfZf6',
+    time: '2023-10-03 17:00',
+    organizer: 'Cloudinary',
+    organizerLink: 'https://google.com',
+    description:
+      'Join us for an evening of cloud optimizing talks and discussions.',
+    meetupLink: 'https://meetup.com/local-aws-meetup',
+    image: 'images/heroimages/4-meetup-image.jpg',
+  },
+});
+
+test('random randomnumber returns number between 1-5', () => {
+  for (let i = 0; i < 100; i++) {
+    const randomNumber: number = getRandomLogonumber();
+    expect(randomNumber).toBeGreaterThanOrEqual(1);
+    expect(randomNumber).toBeLessThanOrEqual(5);
+  }
+});
+
+test('date parsing works in user format', () => {
+  const userDate = '2023-08-25 20:00';
+  const parsedDate = parseMeetupDate(userDate);
+  expect(parsedDate).toBeDefined();
+  expect(parsedDate).not.toBeNull();
+  if (parsedDate) {
+    expect(new Date(parsedDate).toDateString()).toBe('Fri Aug 25 2023');
+  }
+});
+
+test('date parsing works in system format', () => {
+  const systemDate = 'Wed, 13 Sep 2023 16:30:00 GMT';
+  const parsedDate = parseMeetupDate(systemDate);
+  expect(parsedDate).toBeDefined();
+  expect(parsedDate).not.toBeNull();
+  if (parsedDate) {
+    expect(new Date(parsedDate).toUTCString()).toBe(
+      'Wed, 13 Sep 2023 16:30:00 GMT',
+    );
+  }
+});
+
+test('get next meetup from today', () => {
+  const date = new Date(2023, 8, 4, 10, 0);
+  vi.setSystemTime(date);
+  const nextMeetup = getNextMeetup(meetups);
+  expect(nextMeetup?.data.name).toBe('React coding Meetup');
+});
