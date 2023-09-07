025aa7f1af4800b900319e088d0cd615836395a2
Jörge Ojanen <jorge.mhnet@gmail.com>
2023-09-07 17:59:49 +0300

    remove extra part from path (#42)

--- packages/site/astro.config.mjs
+++ packages/site/astro.config.mjs
@@ -1,12 +1,12 @@
-import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://PlanBee-dev.github.io",
-  base: "/oulu-dev-meetups",
+  site: 'https://ouludevmeetup.com',
+  base: '/',
   integrations: [mdx(), sitemap(), tailwind()],
 });
