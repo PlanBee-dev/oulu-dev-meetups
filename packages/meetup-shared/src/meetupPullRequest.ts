b2c5f796b4f15aecf9e6141bfa21c9d52c9dea74
Veeti K <veeti@veetik.com>
2023-09-23 15:05:26 +0300

    accept suggestion about date format
    explain pr desc date format

--- packages/meetup-shared/src/meetupPullRequest.ts
+++ packages/meetup-shared/src/meetupPullRequest.ts
@@ -1,6 +1,17 @@
 import { format } from 'date-fns';
 import { type Meetup } from './meetupType';
 
+const DATE_FORMAT = [
+  // Month as Jan, Feb, ..., Dec
+  'MMM',
+  // Date as 1st, 2nd, ..., 31st
+  'do,',
+  // Year as 0044, 0001, 1900, 2017
+  'yyyy',
+  // Hours and minutes as 01:02
+  'HH:mm',
+].join(' ');
+
 export function getMeetupPullRequestContent(
   meetup: Meetup,
   issueNumber: number,
@@ -8,7 +19,7 @@ export function getMeetupPullRequestContent(
   return `New meetup
 
 Date:
-${format(meetup.date, 'MMM do, yyyy @ HH:mm')}
+${format(meetup.date, DATE_FORMAT)}
 
 Organizer:
 [${meetup.organizer}](${meetup.organizerLink})
