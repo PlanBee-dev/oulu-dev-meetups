dd421aef61b1b95ee897e2b40d6025a6a2e2e7de
AriPerkkio <ari.perkkio@gmail.com>
2023-09-25 20:19:02 +0300

    test: use Astro Node API instead of `node:child_process`

--- packages/site/test/setup.ts
+++ packages/site/test/setup.ts
@@ -1,51 +1,35 @@
-import { ChildProcess, exec, execSync } from 'node:child_process';
 import { resolve } from 'node:path';
 import { fileURLToPath } from 'node:url';
+import { build, preview } from 'astro';
 
-const root = resolve(fileURLToPath(import.meta.url), '../');
+let server: Awaited<ReturnType<typeof preview>>;
 
-let subprocess: ChildProcess;
+const __filename = fileURLToPath(import.meta.url);
+const root = resolve(__filename, '../..');
 
 export async function setup() {
   log('Building astro site');
-  execSync('pnpm build', { cwd: root });
+  await build({ root });
 
-  log('Starting astro server');
-  subprocess = exec('pnpm preview', { cwd: root });
+  const timer = setTimeout(() => {
+    throw new Error('Timeout waiting for Astro preview');
+  }, 10_000);
 
-  await new Promise((resolve, reject) => {
-    const timer = setTimeout(
-      () => reject(new Error('Timeout waiting for "pnpm preview"')),
-      10_000,
-    );
+  log('Starting Astro server 🚀');
+  server = await preview({ root });
 
-    subprocess.stdout?.on('data', (data: Buffer) => {
-      if (data.toString().includes('started in')) {
-        clearTimeout(timer);
-        resolve(null);
-      }
-    });
-
-    subprocess.stderr?.on('data', (data: Buffer) => {
-      reject(data.toString());
-    });
-  });
+  clearTimeout(timer);
 }
 
 export async function teardown() {
-  subprocess.kill();
-  log('Stopping astro server');
+  const timer = setTimeout(() => {
+    throw new Error('Timeout waiting for Astro server to stop');
+  }, 10_000);
 
-  await new Promise((resolve, reject) => {
-    const timer = setTimeout(
-      () => reject(new Error('Timeout waiting for "pnpm preview" to exit')),
-      10_000,
-    );
-    subprocess.on('exit', () => {
-      clearTimeout(timer);
-      resolve(null);
-    });
-  });
+  log('Stopping astro server');
+  await server.stop();
+
+  clearTimeout(timer);
 }
 
 function log(...messages: Parameters<typeof console.log>) {
